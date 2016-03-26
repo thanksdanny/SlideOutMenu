092bb329d31f2a662eeb8b3f94d87419ed1e91cd
thanksdanny <hj-07@163.com>
2016-03-26 16:48:12 +0800

    Initial Commit

---
+++ SlideOutMenu/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SlideOutMenu
+//
+//  Created by Danny Ho on 3/26/16.
+//  Copyright © 2016 thanksdanny. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
